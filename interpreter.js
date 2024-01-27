591adce645f5b606e558ee9803cef7046ce0d6f6
Kieran Llarena <baymax709@gmail.com>
2024-01-27 18:17:24 -0500

    for warren pt2

--- interpreter.js
+++ interpreter.js
@@ -1,42 +1,7 @@
 import fs from 'node:fs'
 import { argv } from 'node:process'
-import { createConstVar, createLetVar, toJSFile } from './core/builder.js'
 
 const fileName = argv[2]
-
-function convertStringToCode(input) {
-  // Split input string into lines
-  const lines = input.split('\n').map(line => line.trim());
-
-  let variableDeclarations = '';
-  let functionDefinition = '';
-
-  // Process each line
-  for (const line of lines) {
-      // Check if the line matches variable declaration pattern
-      const functionDefinitionRegex = /^murmur\s+(\w+)\s*{$/i;
-      const functionMatch = line.match(functionDefinitionRegex);
-      const variableDeclarationRegex = /^meow\s+(\w+)\s+(.+)$/i;
-      const variableMatch = line.match(variableDeclarationRegex);
-
-      if(functionMatch) {
-        const [, functionName] = functionMatch;
-        functionDefinition += `function ${functionName}() {\n`;
-      } else if (line.trim() === '}') {
-        functionDefinition += '}\n';
-      } else if (functionDefinition) {
-        functionDefinition += `  ${line.trim()}\n`;
-      } else if (variableMatch) {
-        const [, variableName, variableValue] = variableMatch;
-        variableDeclarations += `let ${variableName} = ${variableValue}\n`;
-      }
-  }
-
-  // Combine variable declarations and function definition
-  const code = `${variableDeclarations}\n${functionDefinition}`;
-
-  return code;
-}
 
 const writeBuffer = []
 fs.readFile(fileName, 'utf-8', (err, data) => {
@@ -45,7 +10,7 @@ fs.readFile(fileName, 'utf-8', (err, data) => {
     return
   }
 
-  console.log(data.split('\n'))
+  console.log(data.replaceAll(/[\{\}]/g, "bruh"))
   // toJSFile(writeBuffer)
 })
 // const lines = data.split('\n')
