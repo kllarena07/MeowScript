e16b67acfcbfa9e9e2e932e1cf9c3305f29f08e5
Kieran Llarena <baymax709@gmail.com>
2024-01-28 06:59:29 -0500

    asldasd

--- interpreter.js
+++ interpreter.js
@@ -23,10 +23,6 @@ fs.readFile(fileName, 'utf-8', (err, data) => {
                               .replaceAll('!:3', '>')
                               .replaceAll(':>', '>=')
                               .replaceAll('!:>', '<=')
-                              .replaceAll('^.+.^', '+=')
-                              .replaceAll('^.-.^', '-=')
-                              .replaceAll('^.*.^', '*=')
-                              .replaceAll('^./.^', '/=')
                               .replaceAll('^%^', '%')
                               .replaceAll('^+^', '+')
                               .replaceAll('^-^', '-')
@@ -35,30 +31,50 @@ fs.readFile(fileName, 'utf-8', (err, data) => {
                               .split('\n')
 
   for (let line of converted_lines) {
+    if(line.includes('NAP')) {
+      writeBuffer.push(line.replace('NAP', '//'))
+    }
     if (line.includes('meow')) {
       const broken = line.split(' ')
       writeBuffer.push(`let ${broken[1]} = ${broken[2]}`)
-    } else if (line.includes('MEOW')) {
+    }
+    if (line.includes('MEOW')) {
       const broken = line.split(' ')
       writeBuffer.push(`const ${broken[1]} = ${broken[2]}`)
-    } else if (line.includes('hiss')) {
+    }
+    if (line.includes('hiss')) {
       const modified = line.replace('hiss', 'console.log')
       writeBuffer.push(modified)
-    } else if (line.includes('murmur')) {
-      const broken = line.split(' ')
-      writeBuffer.push(`function ${broken[1]}()`)
-    } else if (line.includes('<BLOCK_BEGIN>')) {
+    }
+    if (line.includes('murmur')) {
+      const broken = line.split('murmur')
+      writeBuffer.push(`function ${broken[1].trim()}()`)
+    }
+    if (line.includes('<BLOCK_BEGIN>')) {
       writeBuffer.push(`{`)
-    } else if (line.includes('<BLOCK_END>')) {
-      writeBuffer.push(`}\n`)
-    } else if (line.includes('pet->')) {
+    }
+    if (line.includes('<BLOCK_END>')) {
+      writeBuffer.push(`}`)
+    }
+    if (line.includes('pet->')) {
       const broken = line.split('->')
       writeBuffer.push(`${broken[1]}()`)
-    } else if (line.includes('^o_o^')) {
+    }
+    if (line.includes('^o_o^')) {
       const broken = line.split(' ')
       writeBuffer.push(`while (${broken[1]} ${broken[2]} ${broken[3]})`)
-    } else {
-      writeBuffer.push(line)
+    }
+    if (line.includes('^.+.^')) {
+      writeBuffer.push(line.replace('^.+.^', '+='))
+    }
+    if (line.includes('^.-.^')) {
+      writeBuffer.push(line.replace('^.-.^', '-='))
+    }
+    if (line.includes('^.*.^')) {
+      writeBuffer.push(line.replace('^.*.^', '*='))
+    }
+    if (line.includes('^./.^')) {
+      writeBuffer.push(line.replace('^./.^', '/='))
     }
   }
   toJSFile(writeBuffer)
--- main.js
+++ main.js
@@ -1,27 +1,19 @@
 let test1 = 1
 const test2 = "hello"
-
 test1 += 1
-test1 -= 1
-test1 *= 1
-test1 /= 1
-test1 % 1
-
-function bruh()
+function my_func()
 {
   console.log(test1 + test2)
+function my_nested_func()
+{
+    console.log("i'm nested")
 }
-
-
+my_nested_func()
+}
+my_func()
 let i = 0
 while (i < 5)
 {
   console.log(i)
   i += 1
 }
-
-
-let test3 = 1
-const test4 = 1
-
-console.log(test3 + test4)
